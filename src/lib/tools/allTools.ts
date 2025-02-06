369aa05815d849d75c00e2d1158f57ed866daf23
Chaal Pritam <chaalpritam@gmail.com>
2025-02-06 20:20:20 +0530

    add onchain tools | balance, wallet, send, erc20

--- src/lib/tools/allTools.ts
+++ src/lib/tools/allTools.ts
@@ -1,1 +1,29 @@
-export const tools = {};
+import { getWalletAddressTool } from "./getWalletAddress";
+import { getBalanceTool } from "./getBalance";
+import { sendTransactionTool } from "./sendTransaction";
+import { deployErc20Tool } from "./deployERC20";
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export interface ToolConfig<T = any> {
+    definition: {
+        type: "function";
+        function: {
+            name: string;
+            description: string;
+            parameters: {
+                type: "object";
+                properties: Record<string, unknown>;
+                required: string[];
+            };
+        };
+    };
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    handler: (args: T) => Promise<any>;
+}
+
+export const tools: Record<string, ToolConfig> = {
+    get_balance: getBalanceTool,
+    get_wallet_address: getWalletAddressTool,
+    send_transaction: sendTransactionTool,
+    deploy_erc20: deployErc20Tool
+};
