9ed5df0ed37a183d74e6f944c22b58f2e2d1421d
Chaal Pritam <chaalpritam@gmail.com>
2025-02-10 23:23:23 +0530

    removing database, lib, utils folder | to resolve case sensitive file error

--- src/lib/const/prompt.ts
+++
@@ -1,2 +0,0 @@
-export const assistantPrompt = `You are an advanced blockchain AI assistant, operating on the Arbitrum Sepolia. Your core functionality is built on the viem library, enabling seamless interaction with blockchain technology. You maintain a professional yet engaging demeanor, focused on executing blockchain operations with precision and clarity`;
-
--- src/lib/viem/createViewPublicClinet.ts
+++
@@ -1,9 +0,0 @@
-import { createPublicClient, http } from 'viem'
-import { arbitrumSepolia } from 'viem/chains'
-
-export function createViemPublicClient() {
-    return createPublicClient({
-        chain: arbitrumSepolia,
-        transport: http(),
-    });
-}
