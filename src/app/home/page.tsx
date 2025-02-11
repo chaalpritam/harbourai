73a6bfdaf1e0a1d6e5de77e90d56cfa3efa5305b
Chaal Pritam <chaalpritam@gmail.com>
2025-02-11 22:06:51 +0530

    adding home page bg and content

---
+++ src/app/home/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', background: 'linear-gradient(215.63deg, #F5641C 2.71%, #DC451F 37.53%, #EC5429 72.34%)' }}>
+            <div style={{ textAlign: 'center' }}>
+                <h3 style={{ fontSize: '70px', lineHeight: '80px', fontWeight: 'bold', color: '#fff' }}>every communities foundation <br /> should be a DAO which are <br /> truly decentralized</h3>
+                <p style={{ fontSize: '18px', lineHeight: '26px', fontWeight: 'normal', padding: '30px 0 60px 0', color: '#fff', opacity: 0.75 }}>A better way to manage your sales, team, clients & marketing <br /> — on a single platform. Powerful, affordable & easy.</p>
+            </div>
+        </div>
+    );
+}
